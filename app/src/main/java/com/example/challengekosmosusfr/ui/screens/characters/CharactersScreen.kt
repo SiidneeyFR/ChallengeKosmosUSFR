b3ebf283bd29bfa210c306fb54bf0bcf4f30483e
Ulises Sidney Franco Romero <ulisessfrancor@gmail.com>
2024-03-27 09:27:03 -0700

    init challenge

---
+++ app/src/main/java/com/example/challengekosmosusfr/ui/screens/characters/CharactersScreen.kt
@@ -0,0 +1,4 @@
+package com.example.challengekosmosusfr.ui.screens.characters
+
+fun CharactersScreen() {
+}
---
+++ app/src/main/java/com/example/challengekosmosusfr/ui/screens/characters/CharactersViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.challengekosmosusfr.ui.screens.characters
+
+import androidx.lifecycle.ViewModel
+
+class CharactersViewModel : ViewModel() {
+}
